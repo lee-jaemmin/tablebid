6c1ceea87151694899f44afa6b93a96c9f56f06d
lee-jaemmin <dlwoals8546@gmail.com>
2026-08-13 14:27:51 +0900

    [add] fromJson에 setMenus,setMenuItems 가져오는 코드 추가

--- lib/models/menu_cache_model.dart
+++ lib/models/menu_cache_model.dart
@@ -1,15 +1,22 @@
 import 'package:tablebid/models/category_model.dart';
 import 'package:tablebid/models/item_model.dart';
+import 'package:tablebid/models/set_menu_items_model.dart';
+import 'package:tablebid/models/set_menu_model.dart';
 
 class MenuCacheModel {
     final String companyId;
     final List<CategoryModel> categories;
     final List<ItemModel> items;
+    final List<SetMenuModel> setMenus;
+    final List<SetMenuItemsModel> setMenuItems;
+    
 
     MenuCacheModel({
       required this.companyId,
       required this.categories,
       required this.items,
+      required this.setMenus,
+      required this.setMenuItems,
     });
 
     factory MenuCacheModel.fromJson(Map<String, dynamic> json) {
@@ -21,6 +28,10 @@ class MenuCacheModel {
         items: (json['items'] as List<dynamic>)
             .map((e) => ItemModel.fromJson(e))
             .toList(),
+        setMenus: (json['set_menus'] as List<dynamic>)
+        .map((e)=>SetMenuModel.fromJson(e)).toList(),
+        setMenuItems: (json['set_menu_items'] as List<dynamic>)
+        .map((e)=>SetMenuItemsModel.fromJson(e)).toList(),
       );
     }
   }
