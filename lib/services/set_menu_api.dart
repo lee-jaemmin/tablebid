0b129ecf5e87f7bead3bd3d1572668d15944728a
lee-jaemmin <dlwoals8546@gmail.com>
2026-08-31 16:53:29 +0900

    [feat] 세트메뉴 아이템 가져오는 함수

--- lib/services/set_menu_api.dart
+++ lib/services/set_menu_api.dart
@@ -6,17 +6,17 @@ import 'package:tablebid/screens/set_menu_create_screen.dart';
 import 'package:tablebid/services/api_client.dart';
 
 class SetMenuApi {
-  Future<SetMenuModel> updateSetMenu(
-    {
-      required String companyId,
-      required int setMenuId,
-      bool? isActive,
-      String? setName,
-      int? setPrice,
-      bool? hasMixer,
-    }
-  ) async {
-    final url = Uri.parse('${ApiClient.baseUrl}/companies/${companyId}/set-menus/${setMenuId}');
+  Future<SetMenuModel> updateSetMenu({
+    required String companyId,
+    required int setMenuId,
+    bool? isActive,
+    String? setName,
+    int? setPrice,
+    bool? hasMixer,
+  }) async {
+    final url = Uri.parse(
+      '${ApiClient.baseUrl}/companies/${companyId}/set-menus/${setMenuId}',
+    );
     final body = {
       if (isActive != null) 'is_active': isActive,
       if (setName != null) 'set_name': setName,
@@ -27,15 +27,15 @@ class SetMenuApi {
     final response = await http.patch(
       url,
       headers: {'Content-Type': 'application/json'},
-      body: jsonEncode(body)
+      body: jsonEncode(body),
     );
 
-    if(response.statusCode == 200 || response.statusCode == 201) {
+    if (response.statusCode == 200 || response.statusCode == 201) {
       final Map<String, dynamic> data = jsonDecode(response.body);
       return SetMenuModel.fromJson(data);
     }
     throw Exception(
-      'Failed to modify Set Menu: ${response.statusCode} ${response.body}'
+      'Failed to modify Set Menu: ${response.statusCode} ${response.body}',
     );
   }
 
@@ -55,10 +55,14 @@ class SetMenuApi {
       'set_price': setPrice,
       'is_active': isActive,
       'has_mixer': hasMixer,
-      'items': components.map((component)=> {
-        "item_id": component.itemId,
-        "quantity": component.quantity,
-      }).toList() // toList: Iterable(map) => JSON 배열
+      'items': components
+          .map(
+            (component) => {
+              "item_id": component.itemId,
+              "quantity": component.quantity,
+            },
+          )
+          .toList(), // toList: Iterable(map) => JSON 배열
     };
 
     final response = await http.post(
@@ -73,6 +77,20 @@ class SetMenuApi {
     }
     throw Exception(
       'Failed to create set menu: ${response.statusCode} ${response.body}',
+    );
+  }
+
+  Future<String> getSetMenuItemsBySetMenu({required int setMenuId}) async {
+    final url = Uri.parse('${ApiClient.baseUrl}/set-menus/$setMenuId/items');
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      final data = jsonDecode(response.body) as String;
+      return data;
+    }
+    throw Exception(
+      'Failed to get set menu items by set menu: ${response.statusCode} ${response.body}',
     );
   }
 }
