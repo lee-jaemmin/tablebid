4e78a1438b4420d614e30cdf6c66cff7984e85a3
lee-jaemmin <dlwoals8546@gmail.com>
2026-08-31 12:55:40 +0900

    [chore] alert 디자인 변경

--- lib/widgets/invite_code_card.dart
+++ lib/widgets/invite_code_card.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tablebid/models/company_model.dart';
@@ -6,8 +7,11 @@ import 'package:tablebid/services/company_api.dart';
 class InviteCodeCard extends StatelessWidget {
   final CompanyModel company;
   final ValueChanged<CompanyModel> onCompanyChanged;
-  InviteCodeCard({super.key, required this.company, required this.onCompanyChanged});
-
+  InviteCodeCard({
+    super.key,
+    required this.company,
+    required this.onCompanyChanged,
+  });
 
   // 초대 코드 재발급
   void _showReissueDialog(BuildContext context, String companyId) {
@@ -17,53 +21,63 @@ class InviteCodeCard extends StatelessWidget {
         title: const Text('초대 코드 재발급'),
         content: const Text('코드를 재발급하면 기존 코드는 더 이상 사용할 수 없습니다.\n정말 변경하시겠습니까?'),
         actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(dialogContext),
-            child: const Text('취소', style: TextStyle(color: Colors.black)),
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.red,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadiusGeometry.circular(10),
-              ),
-            ),
-            onPressed: () async {
-              final navigator = Navigator.of(dialogContext);
-              final messenger = ScaffoldMessenger.of(context);
-
-              showDialog(
-                context: context,
-                barrierDismissible: false,
-                builder: (ontext) =>
-                    const Center(child: CircularProgressIndicator()),
-              );
-              // DB 업데이트
-              try {
-                final updatedCompany = await CompanyApi().regenerateInviteCode(companyId: companyId);
-                onCompanyChanged(updatedCompany);
-                navigator.pop(); // 로딩창 끄기
-                navigator.pop(); // 재발급하시겠습니까? 팝업 끄기
-              } catch (e) {
-                navigator.pop(); // 로딩창 끄기
-                messenger.showSnackBar(
-                  const SnackBar(
-                    content: Text('코드 재발급에 실패했습니다. 다시 시도해주세요'),
-                    duration: Duration(seconds: 2),
-                    backgroundColor: Colors.red,
-                    behavior: SnackBarBehavior.floating,
+          Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () => Navigator.pop(dialogContext),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    foregroundColor: Colors.white,
+                    side: BorderSide(color: Colors.white),
                   ),
-                );
-                print("재발급 실패: $e");
-              }
-            },
-            child: const Text(
-              '재발급 실행',
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
+                  child: const Text('취소'),
+                ),
               ),
-            ),
+              SizedBox(width: 12),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                  onPressed: () async {
+                    final navigator = Navigator.of(dialogContext);
+                    final messenger = ScaffoldMessenger.of(context);
+
+                    showDialog(
+                      context: context,
+                      barrierDismissible: false,
+                      builder: (ontext) =>
+                          const Center(child: CupertinoActivityIndicator()),
+                    );
+                    // DB 업데이트
+                    try {
+                      final updatedCompany = await CompanyApi()
+                          .regenerateInviteCode(companyId: companyId);
+                      onCompanyChanged(updatedCompany);
+                      navigator.pop(); // 로딩창 끄기
+                      navigator.pop(); // 재발급하시겠습니까? 팝업 끄기
+                    } catch (e) {
+                      navigator.pop(); // 로딩창 끄기
+                      messenger.showSnackBar(
+                        const SnackBar(
+                          content: Text('코드 재발급에 실패했습니다. 다시 시도해주세요'),
+                          duration: Duration(seconds: 2),
+                          backgroundColor: Colors.red,
+                          behavior: SnackBarBehavior.floating,
+                        ),
+                      );
+                      print("재발급 실패: $e");
+                    }
+                  },
+                  child: const Text(
+                    '재발급 실행',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
