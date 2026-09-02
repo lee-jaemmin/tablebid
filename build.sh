6ba38a9b2cd4a5ba04044e6133562b16eecd5c63
lee-jaemmin <dlwoals8546@gmail.com>
2026-09-02 16:29:37 +0900

    [fix] $추가

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ git clone https://github.com/flutter/flutter.git --depth 1 -b 3.41.3 flutter
 export PATH="$PATH:$(pwd)/flutter/bin"
 
 flutter pub get
-flutter build web --dart-define=API_BASE_URL="API_BASE_URL" --dart-define=JUSO_CONFM_KEY="JUSO_CONFM_KEY"
+flutter build web --dart-define=API_BASE_URL="$API_BASE_URL" --dart-define=JUSO_CONFM_KEY="$JUSO_CONFM_KEY"
