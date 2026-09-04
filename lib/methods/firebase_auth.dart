1fca74c2b1a6e70d5591f7699394f85f0a3bfe04
lee-jaemmin <dlwoals8546@gmail.com>
2026-09-04 12:39:45 +0900

    [mod] 파라미터 추가

--- lib/methods/firebase_auth.dart
+++ lib/methods/firebase_auth.dart
@@ -1,14 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-Future<Map<String, String>> firebaseAuthHeaders() async {
+Future<Map<String, String>> firebaseAuthHeaders({
+  bool forceRefresh = false,
+  bool includeContentType = true,
+  // 이미지 업로드에서는 얘가 false여야함.
+  // multipart가 알아서 보내게 둬야하기 때문.
+  // 'Content-type': 'application/json' 여부를 결정하는 파라미터
+}) async {
   final user = FirebaseAuth.instance.currentUser;
 
   if(user == null) {
     throw Exception('로그인이 필요합니다.');
   }
-  final token = await user.getIdToken();
+  final token = await user.getIdToken(forceRefresh);
   return {
     'Authorization': "Bearer $token",
-    'Content-type': 'application/json'
+    if (includeContentType) 'Content-type': 'application/json'
   };
 }
