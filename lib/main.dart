0c27eaf73d5946a5d3bd5535a69fb664edbd876d
lee-jaemmin <dlwoals8546@gmail.com>
2026-09-07 12:03:51 +0900

    [fix] 폰트명 오타

--- lib/main.dart
+++ lib/main.dart
@@ -136,7 +136,7 @@ class _tablebidState extends State<tablebid> with WidgetsBindingObserver {
     return MaterialApp(
       title: 'tablebid',
       theme: ThemeData(
-        fontFamily: 'NatoSansKR',
+        fontFamily: 'NotoSansKR',
         brightness: Brightness.dark,
         scaffoldBackgroundColor: Colors.black,
         colorScheme: const ColorScheme.dark(
