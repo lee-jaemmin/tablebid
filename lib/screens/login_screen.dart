21239b8fb281688865d804bc17691bb18baecfd0
lee-jaemmin <dlwoals8546@gmail.com>
2026-09-11 13:22:24 +0900

    spacebetween

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -479,6 +479,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
                    const SizedBox(height: 32),
                    Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text('운영사: 그리드(GRID)', style: TextStyle(color: Colors.white),),
                       Text('사업자 등록번호: 262-10-02959',  style: TextStyle(color: Colors.white),),
