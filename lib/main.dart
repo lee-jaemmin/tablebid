4cb1d128b55a863e0095df8646c25c7e20a8abe5
lee-jaemmin <dlwoals8546@gmail.com>
2026-09-22 13:59:32 +0900

    [fix] 폰트 깨짐 최소 수정

--- lib/main.dart
+++ lib/main.dart
@@ -139,6 +139,7 @@ class _tablebidState extends State<tablebid> with WidgetsBindingObserver {
     return MaterialApp(
       title: 'tablebid',
       theme: ThemeData(
+        fontFamily: 'NotoSansKR',
         brightness: Brightness.dark,
         scaffoldBackgroundColor: Colors.black,
         colorScheme: const ColorScheme.dark(
